66e7b207d66b8ee5527a6e9fbd886605a07708a7
salma hamdi <saalmahm51@gmail.com>
2025-10-25 20:29:44 +0100

    feat: ajout du convertisseur JSON Jackson dans WebConfig

---
+++ src/main/java/com/salma/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.salma.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@EnableWebMvc  // Active Spring MVC
+@ComponentScan(basePackages = "com.salma.controller")  // Scan les controllers
+public class WebConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new MappingJackson2HttpMessageConverter());
+    }
+}
